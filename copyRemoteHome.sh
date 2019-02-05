55cde0a8ecdcd8710d15a3874bef31f74cc67aef
Pau Ferrer Vich <pferrervich@gmail.com>
2019-02-05 08:11:06 +0100

    Add files via upload

--- copyRemoteHome.sh
+++ copyRemoteHome.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 echo "EXECUTAR AMB SUPERUSER"
 echo "Introdueix el nom d'usuari que es crearà"
 read USERNAME
@@ -8,11 +9,14 @@ read HOSTNAME
 
 hostnamectl set-hostname $HOSTNAME
 
-echo "127.0.1.1		$HOSTNAME" >> /etc/hosts
+if echo "127.0.1.1		$HOSTNAME" >> /etc/hosts; then
+	echo "S'ha modificat el fitxer hosts"
+else
+	echo "No s'ha pogut modificar el fitxer hosts"
+fi
 
 adduser $USERNAME
 
-echo "Usuari" $USERNAME "creat"
 echo "Introdueix la IP del usuari"
 read USER_IP
 echo "Introdueix el nom del administrador remot"
@@ -21,13 +25,32 @@ read ADMIN_R
 cp -rT /etc/skel /home/$USERNAME
 
 echo "Introdueix la contrasenya d'administrador de l'equip remot"
-ssh -t $ADMIN_R@$USER_IP "sudo chmod 777 -R /home/$USERNAME"
-echo "Ara es copiara la carpeta HOME de l'usuari"
-sleep 5s
-rsync -r -a -v -e ssh $ADMIN_R@$USER_IP:/home/$USERNAME/  /home/$USERNAME/
+if ssh -t $ADMIN_R@$USER_IP "sudo chmod 777 -R /home/$USERNAME"; then
+  echo "S'han canviat els permisos de la carpeta remota"
+else
+  echo "No s'ha pogut canviar el chmod de la carpeta d'usuari remota"
+fi
+sleep 2s
 
-chown -R $USERNAME:$USERNAME /home/$USERNAME
-chmod -R 755 /home/$USERNAME
+echo "Es copiara la carpeta HOME de l'usuari"
+sleep 4s
+if rsync -r -a -v -e ssh $ADMIN_R@$USER_IP:/home/$USERNAME/  /home/$USERNAME/; then
+  echo "S'ha copiat la carpeta de l'usuari"
+else
+  echo "No s'ha pogut copiar la carpeta remota de l'usuari"
+fi
+
+if chown -R $USERNAME:$USERNAME /home/$USERNAME; then
+  echo "S'han canviat els permisos chown de la carpeta"
+else
+  echo "No s'han pogut canviar els permisos chown de la carpeta"
+fi
+
+if chmod -R 755 /home/$USERNAME; then
+  echo "S'han canviat els permisos chmod de la carpeta"
+else
+  echo "No s'han pogut canviar els permisos chmod de la carpeta"
+fi
 
 ifconfig eth2 $USER_IP netmask 255.255.255.0
 echo "Quin gatweay ha de tenir?"
@@ -35,8 +58,12 @@ read GW
 route add default gw $GW eth2
 echo "Configurant xarxa..."
 
+if sed -i '1s/.*/LANGUAGE        DEFAULT=ca:es/' /home/$USERNAME/.pam_environment; then
+  echo "S'ha canviat l'idioma del sistema"
+else
+  echo "No s'ha pogut canviar l'idioma del sistema"
+fi
 
-sed -i '1s/.*/LANGUAGE        DEFAULT=ca:es/' /home/$USERNAME/.pam_environment
 echo "##########################################################"
 echo "########  ATENCIÓ: COMPROVAR MANUALMENT LA XARXA. ########"
 echo "### ES POT DONAR EL CAS DE QUE NO CANVÏ AUTOMATICAMENT ###"
