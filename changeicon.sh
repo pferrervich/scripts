7b2868607cd04915c36dd1438aa008bb734aa494
Pau Ferrer Vich <pferrervich@gmail.com>
2022-12-24 12:27:53 +0100

    Delete changeicon.sh

--- changeicon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-#Creates a new file in /tmp/ where the line number 210 will be changed with the new icon URI, and then replaces
-#the original file in /usr/share/applications/.
-#The row number can be changed with the one of your system language to take effect.
-sed '210 c\Icon[es]=/home/poh/Imagenes/Pictures/Icons/mpm.png' /usr/share/applications/eog.desktop > /tmp/eog.desktop
-mv /tmp/eog.desktop /usr/share/applications/
-
